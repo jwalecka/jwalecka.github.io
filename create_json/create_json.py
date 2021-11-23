c3155e4d59db2a87e051f1ac9b46e32d510c9a0f
jwalecka <john.walecka@gmail.com>
2021-11-23 11:50:18 -0800

    updated excel file and ran python script

--- create_json/create_json.py
+++ create_json/create_json.py
@@ -1,19 +1,21 @@
 import pandas as pd
-from collections import defaultdict
 import json
-from classes import DefaultOrderedDict
 
 filename = "musicsketch_faqs.xlsx"
 df = pd.read_excel(filename, header=None, names=['section', 'question', 'answer'])
-sections_dict = DefaultOrderedDict(dict)
+section_names = df['section'].unique()
+json_array = []
 
-for index, row in df.iterrows():
-    current_dict = sections_dict[row['section']]
-    current_dict[len(current_dict)] = {'question': row['question'], 'answer': row['answer']}
-    sections_dict[row['section']] = current_dict
+for section_name in section_names:
+    section_dict = dict()
+    section_dict['section_name'] = section_name
+    question_array = list()
+    for index, row in df[df['section'] == section_name].iterrows():
+        question_array.append({'question': row['question'], 'answer': row['answer']})
+    section_dict['entries'] = question_array
+    json_array.append(section_dict)
 
-json_object = json.dumps(sections_dict)
+json_object = json.dumps(json_array)
 jsonFile = open("faqs.json", "w")
 jsonFile.write(json_object)
-print(json_object)
 jsonFile.close()
